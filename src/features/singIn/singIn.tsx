b1a37350436714a7e7c6809dcb5dd5aec0a2eb21
NadzeyaValatkevich <nadkonadzya@rambler.ru>
2022-07-24 00:46:40 +0300

    added styles login page

--- src/features/singIn/singIn.tsx
+++ src/features/singIn/singIn.tsx
@@ -1,45 +1,47 @@
 import React from 'react';
-import Grid from '@mui/material/Grid';
 import FormControl from '@mui/material/FormControl';
 import Typography from '@mui/material/Typography';
-import FormLabel from '@mui/material/FormLabel';
 import TextField from '@mui/material/TextField';
-import FormGroup from '@mui/material/FormGroup';
 import Button from '@mui/material/Button';
-import style from './SignIn.module.css'
+import style from './SignIn.module.css';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Paper from '@mui/material/Paper';
 
 
 export const SingIn = () => {
     return (
         <div className={style.loginBlock}>
-
-            <Typography variant={'h4'}>
-                SIGN IN
-            </Typography>
-            <form className={style.loginForm}>
-                <FormControl>
-                    <TextField label={'Email'}
-                               margin={'normal'}
-                               variant="standard"
-                    />
-                    <TextField label={'Password'}
-                               margin={'normal'}
-                               variant="standard"
-                    />
-                    <Button variant={'text'} size={'small'} style={{color: 'black'}}>
-                        Forgot Password
-                    </Button>
-                    <Button type={'submit'} variant={'contained'} color={'primary'}>
-                        Login
-                    </Button>
-                    <Typography variant={'subtitle2'} component={'div'} style={{color: 'gray'}}>
-                        Don't have an account?
-                    </Typography>
-                    <Button variant={'text'} color={'primary'}>
-                        Sign Up
-                    </Button>
-                </FormControl>
-            </form>
+            <Paper elevation={3} className={style.loginBlockForm}>
+                <Typography variant={'h4'}>
+                    SIGN IN
+                </Typography>
+                <form className={style.loginForm}>
+                    <FormControl style={{width: '100%'}}>
+                        <TextField label={'Email'}
+                                   margin={'normal'}
+                                   variant="standard"
+                        />
+                        <TextField label={'Password'}
+                                   margin={'normal'}
+                                   variant="standard"
+                        />
+                        <FormControlLabel label={'Remember me'} control={<Checkbox/>}/>
+                        <Button variant={'text'} size={'small'} className={style.btnForgotPass}>
+                            Forgot Password
+                        </Button>
+                        <Button type={'submit'} variant={'contained'} color={'primary'} style={{marginTop:'100px'}}>
+                            Login
+                        </Button>
+                        <Typography variant={'subtitle2'} component={'div'} className={style.textQuestion}>
+                            Don't have an account?
+                        </Typography>
+                        <Button variant={'text'} color={'primary'} >
+                            Sign Up
+                        </Button>
+                    </FormControl>
+                </form>
+            </Paper>
         </div>
     );
 };
