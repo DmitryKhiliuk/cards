c12ef80fa08a9f3fe4d62593f0b99f9eeff5dea7
Tsyriulnikov <max_djvu@mail.ru>
2022-08-02 14:43:42 +0300

    cardParck-reducer2_08_22 1

--- src/features/CardsPack/CardsPack.tsx
+++ src/features/CardsPack/CardsPack.tsx
@@ -4,22 +4,25 @@ import Paper from "@mui/material/Paper";
 import style from './CardsPack.module.css'
 import {HeaderCardsPack} from "./packsList/header/HeaderCardsPack";
 import {PaginationCardsPack} from "./packsList/footer/PaginationCardsPack";
-import {getPacksTC} from "./cardsPack-reducer";
+import {getPacksTC, getStartPacksTC} from "./cardsPack-reducer";
 import {useDispatch} from "react-redux";
 import {ThunkDispatch} from "redux-thunk";
 import {AppDispatch, AppRootStateType} from "../../app/store";
 import {Action} from "redux";
+import {ErrorSnackbar} from "../../utils/ErrorSnackbar/ErrorSnackbar";
 
 export const CardsPack = () => {
     const dispatch = useDispatch<ThunkDispatch<AppRootStateType, unknown, Action> & AppDispatch>()
-    const [packNameSearch,setPackNameSearch]=useState('')
+    // const [packNameSearch,setPackNameSearch]=useState('')
 
     useEffect(() => {
-        dispatch(getPacksTC({packName:packNameSearch}))
+        // dispatch(getPacksTC({packName:packNameSearch}))
+        dispatch(getStartPacksTC())
     }, [])
 
     return (
             <div className={style.blockTable}>
+                <ErrorSnackbar/>
                 <HeaderCardsPack/>
                 <TableList/>
                 <PaginationCardsPack/>
