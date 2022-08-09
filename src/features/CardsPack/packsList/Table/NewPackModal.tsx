e0b7f453f92a823fc8e963868a00d841f3e37e73
DmitryKhiliuk <khiliukbrest@gmail.com>
2022-08-09 12:38:42 +0300

    update 09.08 modal add pack all

--- src/features/CardsPack/packsList/Table/NewPackModal.tsx
+++ src/features/CardsPack/packsList/Table/NewPackModal.tsx
@@ -1,50 +1,38 @@
-import React, {ChangeEvent, useState} from 'react';
+import React, {ChangeEvent, SyntheticEvent, useState} from 'react';
 import {BasicModal} from "../../../../common/modal/modal";
 import TextField from "@mui/material/TextField";
-import Typography from "@mui/material/Typography";
-import CloseIcon from '@mui/icons-material/Close';
-import IconButton from "@mui/material/IconButton";
 import Checkbox from "@mui/material/Checkbox";
-import Button from "@mui/material/Button";
 import FormControlLabel from "@mui/material/FormControlLabel";
 
 type NewPackModalType = {
-    addPack: (name: string) => void
+    addPack: (name: string, privatePack: boolean) => void
 }
 
 export const NewPackModal = (props:NewPackModalType) =>  {
 
     const [title, setTitle] = useState('')
     const [open, setOpen] = React.useState(false);
+    const [checked, setChecked] = useState(false)
 
-    const onChangeHandler = (e:ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const onChangeCheckboxHandler = (e:ChangeEvent<HTMLInputElement>) => {
+        setChecked(e.currentTarget.checked)
+    }
+
+    const onChangeTextFieldHandler = (e:ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         setTitle(e.currentTarget.value)
     }
-
-
-    const onClickSaveHandler = () => {
-        props.addPack(title)
+    const addPackHandler = () => {
+        props.addPack(title, checked)
+        setChecked(false)
         setOpen(false)
     }
-
-
-    const onClickCloseHandler = () => {
-        setOpen(false)
-    }
-
     return (
-        <BasicModal name={'Add new pack'} open={open} setOpen={setOpen}>
-
-            <TextField onChange={onChangeHandler} id="standard-basic" label="Name Pack" variant="standard" />
+        <BasicModal name={'Add new pack'} open={open} setOpen={setOpen} onSave={addPackHandler}>
+            <TextField onChange={onChangeTextFieldHandler} id="standard-basic" label="Name Pack" variant="standard" />
             <div >
-                <FormControlLabel control={<Checkbox
-                />} label="Private Pack" />
+                <FormControlLabel  control={<Checkbox
+                    onChange={onChangeCheckboxHandler}/>} label="Private Pack" />
             </div>
-            <div style={{display: 'flex', justifyContent: 'space-between', marginTop: '10px'}}>
-                <Button variant="contained" onClick={onClickCloseHandler}>Cancel</Button>
-                <Button variant="contained" onClick={onClickSaveHandler}>Save</Button>
-            </div>
-
         </BasicModal>
     );
 }
